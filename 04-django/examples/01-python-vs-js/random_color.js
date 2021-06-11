4cfc6dc2fe79018d1854e6d3fa15788040977f7e
Matthieu Bovel <matthieu@bovel.net>
2021-06-11 08:39:38 +0200

    Add random_color example

---
+++ 04-django/examples/01-python-vs-js/random_color.js
@@ -0,0 +1,6 @@
+const colors = ["red", "green", "orange", "pink"];
+
+const randomColor = () => colors[Math.floor(Math.random() * colors.length)]
+
+console.log(randomColor())
+console.log(randomColor())
