4cdcd784d9433b9bd23f0a9804f4ce8b0525105f
Matthieu Bovel <matthieu@bovel.net>
2021-06-13 19:45:35 +0200

    Split Python and Django chapters

--- 04-django/examples/01-python-vs-js/random_color.py
+++
@@ -1,9 +0,0 @@
-import random
-
-colors = ["red", "green", "orange", "pink"];
-
-def randomColor():
-        return colors[random.randint(0, len(colors) - 1)]
-
-print(randomColor())
-print(randomColor())
