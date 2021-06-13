b34224f10d59bc6e755ef1c6883d4a2e063cb94c
Marc Sommerhalder <msom@gmx.ch>
2021-06-13 22:01:27 +0200

    Make sure at least one digit is selected

--- 04-python/solutions/generate_password_constrained2.py
+++ 04-python/solutions/generate_password_constrained2.py
@@ -12,7 +12,7 @@ def generate_password_constrained2():
     size = randint(10, 20)
     n_digits = randint(1, size-2)
     password = choices(letters, k=size - n_digits) + \
-        choices(digits, k=n_digits - 1) + \
+        choices(digits, k=n_digits) + \
         choices(special_chars, k=1)
     shuffle(password)
     return "".join(password)
