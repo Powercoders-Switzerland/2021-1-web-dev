3a4c469d0f448d3f32cd69bb9acd61df61008339
Marc Sommerhalder <msom@gmx.ch>
2021-06-13 21:41:45 +0200

    Use choices instead of sample in generate_password_constrained2

--- 04-python/solutions/generate_password_constrained2.py
+++ 04-python/solutions/generate_password_constrained2.py
@@ -1,4 +1,4 @@
-from random import randint, sample, shuffle
+from random import randint, choices, shuffle
 import string
 
 # See https://www.erklär-mir-mal.ch/2021-1/09-03-more-on-python.html#/4/0/0
@@ -11,9 +11,9 @@ special_chars = '_-'
 def generate_password_constrained2():
     size = randint(10, 20)
     n_digits = randint(1, len(digits))
-    password = sample(letters, size - n_digits) + \
-        sample(digits, n_digits - 1) + \
-        sample(special_chars, 1)
+    password = choices(letters, k=size - n_digits) + \
+        choices(digits, k=n_digits - 1) + \
+        choices(special_chars, k=1)
     shuffle(password)
     return "".join(password)
 
