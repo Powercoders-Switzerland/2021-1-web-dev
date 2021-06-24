5bc370c55cb752dcb436904e42ab160398cb932c
Marc Sommerhalder <marc.sommerhalder@ost.ch>
2021-06-24 14:08:46 +0200

    Add missing filter settings

--- 06-shop-project/boilerplate/back/webservice/settings.py
+++ 06-shop-project/boilerplate/back/webservice/settings.py
@@ -128,3 +128,11 @@ STATIC_ROOT = BASE_DIR.joinpath('static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#  Django REST framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ]
+}
